3f808cd9dcb937a10f098380ea51052c2d71eb20
rafal423 <30475226+rafal423@users.noreply.github.com>
2023-01-01 19:59:41 +0100

    Lab 5

--- strona/polls/models.py
+++ strona/polls/models.py
@@ -3,6 +3,10 @@ import datetime
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 class Druzyna(models.Model):
     nazwa = models.CharField(max_length=50)
@@ -33,6 +37,11 @@ class Osoba(models.Model):
         GRUDZIEN = 12, ("Grudzień")
     miesiac_urodzenia = models.IntegerField(choices=Miesiace.choices, default=timezone.now().month)
     druzyna = models.ForeignKey(Druzyna, null=True, blank=True, on_delete=models.SET_NULL)
+
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def create_auth_token(sender, instance=None, created=False, **kwargs):
+        if created:
+            Token.objects.create(user=instance)
 
     def __str__(self):
         return '%s %s' % (self.imie, self.nazwisko)
